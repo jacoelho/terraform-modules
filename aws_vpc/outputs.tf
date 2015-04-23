6b7c207155aa8b4b1f814310d0f9bdf592f18328
jacoelho <jose.coelho@linkedcare.com>
2015-04-23 18:04:18 +0100

    fix subnet id

--- aws_vpc/outputs.tf
+++ aws_vpc/outputs.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
 }
 
 output "public_subnet_1_id" {
-  value = "${aws_vpc.public_a.id}"
+  value = "${aws_subnet.public_a.id}"
 }
 
 output "public_subnet_2_id" {
-  value = "${aws_vpc.public_b.id}"
+  value = "${aws_subnet.public_b.id}"
 }
 
 output "private_subnet_1_id" {
-  value = "${aws_vpc.private_a.id}"
+  value = "${aws_subnet.private_a.id}"
 }
 
 output "private_subnet_2_id" {
-  value = "${aws_vpc.private_b.id}"
+  value = "${aws_subnet.private_b.id}"
 }
