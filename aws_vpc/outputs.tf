43ca9904df0620e23d44911da64613d0858a9664
jacoelho <jose.coelho@linkedcare.com>
2015-04-23 17:54:59 +0100

    output subnet ids

--- aws_vpc/outputs.tf
+++ aws_vpc/outputs.tf
@@ -1,3 +1,19 @@
 output "vpc_id" {
   value = "${aws_vpc.default.id}"
 }
+
+output "public_subnet_1_id" {
+  value = "${aws_vpc.public_a.id}"
+}
+
+output "public_subnet_2_id" {
+  value = "${aws_vpc.public_b.id}"
+}
+
+output "private_subnet_1_id" {
+  value = "${aws_vpc.private_a.id}"
+}
+
+output "private_subnet_2_id" {
+  value = "${aws_vpc.private_b.id}"
+}
